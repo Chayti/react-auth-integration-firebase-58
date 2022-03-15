19f5ca4a548c5c969f0817f5aee8a22f8103cb94
Chayti Saha <chaytisaha98@gmail.com>
2022-03-15 14:59:24 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Header from './components/Header/Header';
+import AuthProvider from './context/AuthProvider';
+import Shipping from './components/Shipping/Shipping';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import PlaceOrder from './components/PlaceOrder/PlaceOrder';
+
+function App() {
+  return (
+    <div className="App">
+      <AuthProvider>
+        <BrowserRouter>
+          <Header></Header>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/shipping" element={<PrivateRoute><Shipping /></PrivateRoute>} />
+            <Route path="/placeorder" element={<PrivateRoute><PlaceOrder /></PrivateRoute>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAG8O_IrGGHSGKl_dQ5e4NteZSR0Xowda0",
+    authDomain: "simple-firebase-auth-578e0.firebaseapp.com",
+    projectId: "simple-firebase-auth-578e0",
+    storageBucket: "simple-firebase-auth-578e0.appspot.com",
+    messagingSenderId: "431308382026",
+    appId: "1:431308382026:web:0b9d7ab2d1ad157f2e6bb3"
+};
+
+export default firebaseConfig;
